fd06b2140b743b3f1de3f948aee4d228e9aa3c4e
Kirill <krlee322@gmail.com>
2024-01-19 15:54:25 +0600

    toggle

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,28 +1,40 @@
+// var slideIndex = 1;
+// showSlides(slideIndex);
 
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
 
-var slideIndex = 1;
-showSlides(slideIndex);
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// function showSlides(n) {
+//   var i;
+//   var slides = document.getElementsByClassName("review__article");
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("review__article");
-
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n<1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i<slides.length; i++){
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
-}
+//   if (n > slides.length) {
+//     slideIndex = 1
+//   }
+//   if (n<1) {
+//     slideIndex = slides.length
+//   }
+//   for (i = 0; i<slides.length; i++){
+//     slides[i].style.display = "none";
+//   }
+//   slides[slideIndex-1].style.display = "block";
+// }
+    var accItem = document.getElementsByClassName('accordionItem');
+    var accHD = document.getElementsByClassName('accordionItemHeading');
+    for (i = 0; i < accHD.length; i++) {
+        accHD[i].addEventListener('click', toggleItem, false);
+    }
+    function toggleItem() {
+        var itemClass = this.parentNode.className;
+        for (i = 0; i < accItem.length; i++) {
+            accItem[i].className = 'accordionItem close';
+        }
+        if (itemClass == 'accordionItem close') {
+            this.parentNode.className = 'accordionItem open';
+        }
+    }
